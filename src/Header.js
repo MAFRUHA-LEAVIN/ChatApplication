e39a655d781c264e617ac492ad9e4c3d22b19867
Mafruha Nuhary <mafruha.nuhary@tuni.fi>
2020-12-07 13:46:57 +0200

    2nd

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,16 @@
 import React from "react";
+import chaticon4 from "../src/chaticon4.png";
+
 
 function Header() {
   return (
-    <header>
-      <h1>Chat Application</h1>
+    <header >
+      
+ 
+
+      
+      <h1>
+Chat Application</h1>
     </header>
   );
 }
