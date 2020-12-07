c1d7477508cc1b845a0c50fbfcaeb07b38448322
Mafruha Nuhary <mafruha.nuhary@tuni.fi>
2020-12-07 12:22:50 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from "./Header";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+      <body className="App-header">
+      
+      </body>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
